36ffa9d9d953a018c2081cb553da969a37da9b50
Alexey Zhuchkov <zer0divisi1@gmail.com>
2012-10-21 22:11:10 +0400

    Initial Commit

---
+++ FinderMenu/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FinderMenu
+//
+//  Created by Alexey Zhuchkov on 10/21/12.
+//  Copyright (c) 2012 InfiniteLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
