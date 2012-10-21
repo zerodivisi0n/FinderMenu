aac39451203ceb5b1202e55a7e0b260654299f44
Alexey Zhuchkov <zer0divisi1@gmail.com>
2012-10-21 23:52:51 +0400

    Obtaining Finder PID

--- FinderMenu/main.m
+++ FinderMenu/main.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2012 InfiniteLabs. All rights reserved.
 //
 
+#import <AppKit/AppKit.h>
 #import <Foundation/Foundation.h>
 
 #import "mach_inject_bundle/mach_inject_bundle.h"
@@ -14,14 +15,20 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        pid_t pid = 100;  // TODO: get Finder pid
+        NSArray *apps = [NSRunningApplication runningApplicationsWithBundleIdentifier:@"com.apple.finder"];
+        if ([apps count] < 1) {
+            NSLog(@"Finder not found");
+            return -1;
+        }
+
+        // Use first found app
+        pid_t pid = [[apps objectAtIndex:0] processIdentifier];
 
         NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"FinderExt" ofType:@"bundle"];
         
         NSLog(@"Bundle path: %@", bundlePath);
         NSLog(@"PID: %d", pid);
-        
+
         mach_error_t err;
         err = mach_inject_bundle_pid([bundlePath fileSystemRepresentation], pid);
         NSLog(@"Inject result: %d", err);
