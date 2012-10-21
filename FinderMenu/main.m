01b491e505708707e0998ccbb4cc76b10cecc68b
Alexey Zhuchkov <zer0divisi1@gmail.com>
2012-10-21 23:33:01 +0400

    Added basic injection code

--- FinderMenu/main.m
+++ FinderMenu/main.m
@@ -8,14 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+#import "mach_inject_bundle/mach_inject_bundle.h"
+
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        pid_t pid = 100;  // TODO: get Finder pid
+
+        NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"FinderExt" ofType:@"bundle"];
         
+        NSLog(@"Bundle path: %@", bundlePath);
+        NSLog(@"PID: %d", pid);
+        
+        mach_error_t err;
+        err = mach_inject_bundle_pid([bundlePath fileSystemRepresentation], pid);
+        NSLog(@"Inject result: %d", err);
     }
     return 0;
 }
