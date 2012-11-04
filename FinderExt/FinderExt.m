1c17174475546ca7764a95b6ddb5d12773888784
Alexey Zhuchkov <zer0divisi1@gmail.com>
2012-11-05 02:10:17 +0400

    Rewritten implementation of context menu injection
    Now based again on Method Swizzle for method
    `configureWithNodes:browserController:container:` of class
    `TContextMenu`

--- FinderExt/FinderExt.m
+++ FinderExt/FinderExt.m
@@ -10,63 +10,84 @@
 
 #import <objc/runtime.h>
 
-#import "mach_override.h"
+#import "Finder.h"
+#import "TFENodeHelper.h"
+
+static TFENodeHelper *gNodeHelper;
 
 // Auxiliary class to execute menu actions
-@interface MenuItemTarget : NSObject
+@interface MenuItemTarget : NSObject {
+    @private
+    NSArray *_files;
+}
+- (id)initWithNodes:(const struct TFENodeVector *)vector;
 - (void)sayHello:(id)sender;
 - (void)sayGoodbye:(id)sender;
 @end
 
 @implementation MenuItemTarget
-- (void)sayMessage:(NSString *)message {
-    [[NSAlert alertWithMessageText:@"FinderExt"
+- (id)initWithNodes:(const struct TFENodeVector *)vector {
+    self = [super init];
+    if (self) {
+        NSInteger capacity = vector->_end - vector->_begin;
+        NSMutableArray *files = [[NSMutableArray alloc] initWithCapacity:capacity];
+        struct TFENode *node;
+        for (node = vector->_begin; node < vector->_end; ++node) {
+            [files addObject: [gNodeHelper pathForNode:node]];
+        }
+        _files = files;
+    }
+    return self;
+}
+
+- (void)sayMessage:(NSString *)title {
+    [[NSAlert alertWithMessageText:title
                     defaultButton:nil
                   alternateButton:nil
                       otherButton:nil
-        informativeTextWithFormat:@"Say %@", message] runModal];
+        informativeTextWithFormat:@"Files: %@", _files] runModal];
+    
+    [self release];
 }
+
 - (void)sayHello:(id)sender { [self sayMessage:@"Hello"]; }
 - (void)sayGoodbye:(id)sender { [self sayMessage:@"Goodbye"]; }
 @end
 
-typedef void (*BuildContextMenuProc)(Class, SEL, NSMenu *, unsigned int, id, unsigned long long, BOOL);
-
-static BuildContextMenuProc gOrigContextMenuProc;
-static MenuItemTarget *gMenuItemTarget;
-
-void override_buildContextMenu(Class c, SEL s, NSMenu *menu, unsigned int context, id browserController, unsigned long long maxItems, BOOL addServices) {
-    NSLog(@"In override method");
-    gOrigContextMenuProc(c, s, menu, context, browserController, maxItems, addServices);
+/**
+ * Renames the selector for a given method.
+ * Searches for a method with origSEL and reassigned overrideSEL to that
+ * implementation.
+ * http://www.mikeash.com/pyblog/friday-qa-2010-01-29-method-replacement-for-fun-and-profit.html
+ */
+static void MethodSwizzle(Class c, SEL origSEL, SEL overrideSEL)
+{
+    Method origMethod = class_getInstanceMethod(c, origSEL);
+    Method overrideMethod = class_getInstanceMethod(c, overrideSEL);
     
-    // Find first separator to insert menu after it
-    NSInteger index;
-    for (index = 1 /* 0 is always separator  */; index < [menu numberOfItems]; ++index) {
-        if ([[menu itemAtIndex:index] isSeparatorItem]) {
-            // separator found!
-            break;
-        }
+    NSLog(@"orig=%p, override=%p", origMethod, overrideMethod);
+    
+    if(class_addMethod(c, origSEL, method_getImplementation(overrideMethod), method_getTypeEncoding(overrideMethod))) {
+        class_replaceMethod(c, overrideSEL, method_getImplementation(origMethod), method_getTypeEncoding(origMethod));
+    } else {
+        method_exchangeImplementations(origMethod, overrideMethod);
     }
-    
-    NSLog(@"Separator found at index: %ld", index);
-    
-    // Build extension menu
-    NSMenuItem *myMenuItem = [[NSMenuItem alloc] initWithTitle:@"My Menu" action:nil keyEquivalent:@""];
-    NSMenu *mySubmenu = [[NSMenu alloc] initWithTitle:@"My menu"];
-    [mySubmenu setAutoenablesItems:NO];
-    [[mySubmenu addItemWithTitle:@"Say Hello"
-                          action:@selector(sayHello:)
-                   keyEquivalent:@""]
-     setTarget:gMenuItemTarget];
-    [[mySubmenu addItemWithTitle:@"Say Goodbye"
-                          action:@selector(sayGoodbye:)
-                   keyEquivalent:@""]
-     setTarget:gMenuItemTarget];
-    [myMenuItem setSubmenu:mySubmenu];
-    
-    // Add menu
-    [menu insertItem:myMenuItem atIndex:index + 1];
-    [menu insertItem:[NSMenuItem separatorItem] atIndex:index + 2];
+}
+
+static void OverrideClass(const char *name, SEL origSEL, Method overrideMethod) {
+    Class c = objc_getClass(name);
+    if (c != nil) {
+        // add override method to target class
+        class_addMethod(c,
+                        method_getName(overrideMethod),
+                        method_getImplementation(overrideMethod),
+                        method_getTypeEncoding(overrideMethod));
+        // swizzle methods
+        MethodSwizzle(c, origSEL, method_getName(overrideMethod));
+        NSLog(@"Method 'menuForEvent:' overriden in class %s", name);
+    } else {
+        NSLog(@"Class %s not found to override", name);
+    }
 }
 
 @implementation FinderExt
@@ -80,29 +101,54 @@ void override_buildContextMenu(Class c, SEL s, NSMenu *menu, unsigned int contex
     
     NSLog(@"Main bundle: %@", [[NSBundle mainBundle] bundleIdentifier]);
     
-    Class class_TContextMenu = objc_getClass("TContextMenu");
-    if (class_TContextMenu != nil) {
-        // Create menu item target
-        gMenuItemTarget = [[MenuItemTarget alloc] init];
-        
-        // Try to override method
-        Method original_buildContextMenu = class_getClassMethod(class_TContextMenu, @selector(buildContextMenu:forContext:browserController:maxItems:addServices:));
-        if (original_buildContextMenu != nil) {
-            mach_error_t err;
-            err = mach_override_ptr(method_getImplementation(original_buildContextMenu), override_buildContextMenu, (void**)&gOrigContextMenuProc);
-            if (err == err_none) {
-                NSLog(@"Override successful!");
-            } else {
-                NSLog(@"Override error: %d", err);
-            }
-        } else {
-            NSLog(@"Method 'buildContextMenu:forContext:browserController:maxItems:addServices:' not found in 'TContextMenu' class");
-        }
-    } else {
-        NSLog(@"Class 'TContextMenu' not found in process");
+    // Create helper object
+    gNodeHelper = [[TFENodeHelper alloc] init];
+    if (gNodeHelper == nil) {
+        NSLog(@"Failed to instantiate 'TFENodeHelper' class");
+        return;
     }
     
+    Method method = class_getInstanceMethod(self, @selector(override_configureWithNodes:browserController:container:));
+    OverrideClass("TContextMenu", @selector(configureWithNodes:browserController:container:), method);
+    
     NSLog(@"FinderExt load is complete");
+}
+
+- (void)override_configureWithNodes:(const struct TFENodeVector *)vector
+                  browserController:(id)browserController
+                          container:(BOOL)container {
+    [self override_configureWithNodes:vector browserController:browserController container:container];
+    
+    NSMenu *contextMenu = (NSMenu *)self;
+    
+    // Find first separator to insert menu after it
+    NSInteger index;
+    for (index = 1 /* 0 is always separator  */; index < [contextMenu numberOfItems]; ++index) {
+        if ([[contextMenu itemAtIndex:index] isSeparatorItem]) {
+            // separator found!
+            break;
+        }
+    }
+    
+    MenuItemTarget *myMenuTarget = [[MenuItemTarget alloc] initWithNodes:vector];
+    
+    // Build extension menu
+    NSMenuItem *myMenuItem = [[NSMenuItem alloc] initWithTitle:@"My Menu" action:nil keyEquivalent:@""];
+    NSMenu *mySubmenu = [[NSMenu alloc] initWithTitle:@"My menu"];
+    [mySubmenu setAutoenablesItems:NO];
+    [[mySubmenu addItemWithTitle:@"Say Hello"
+                          action:@selector(sayHello:)
+                   keyEquivalent:@""]
+     setTarget:myMenuTarget];
+    [[mySubmenu addItemWithTitle:@"Say Goodbye"
+                          action:@selector(sayGoodbye:)
+                   keyEquivalent:@""]
+     setTarget:myMenuTarget];
+    [myMenuItem setSubmenu:mySubmenu];
+    
+    // Add menu
+    [contextMenu insertItem:myMenuItem atIndex:index + 1];
+    [contextMenu insertItem:[NSMenuItem separatorItem] atIndex:index + 2];
 }
 
 @end
