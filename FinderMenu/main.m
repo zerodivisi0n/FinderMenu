5e31e6a9a1bc91ff96fd63f7083871771c98152f
Alexey Zhuchkov <zer0divisi1@gmail.com>
2013-03-24 16:36:06 +0400

    Update loader

--- FinderMenu/main.m
+++ FinderMenu/main.m
@@ -13,30 +13,29 @@
 
 int main(int argc, const char * argv[])
 {
-
-    @autoreleasepool {
-        NSArray *apps = [NSRunningApplication runningApplicationsWithBundleIdentifier:@"com.apple.finder"];
-        if ([apps count] < 1) {
-            NSLog(@"Finder not found");
-            return -1;
-        }
-
-        // Use first found app
-        pid_t pid = [[apps objectAtIndex:0] processIdentifier];
-
-        NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"FinderExt" ofType:@"bundle"];
-        
-        NSLog(@"Bundle path: %@", bundlePath);
-        NSLog(@"PID: %d", pid);
-
-        mach_error_t err;
-        err = mach_inject_bundle_pid([bundlePath fileSystemRepresentation], pid);
-        if (err == err_none) {
-            NSLog(@"Inject successful!");
-        } else {
-            NSLog(@"Inject error: %d", err);
-        }
+  @autoreleasepool {
+    NSArray *apps = [NSRunningApplication runningApplicationsWithBundleIdentifier:@"com.apple.finder"];
+    if ([apps count] < 1) {
+      NSLog(@"Finder not found");
+      return -1;
     }
-    return 0;
+    
+    // Use first found app
+    pid_t pid = [[apps objectAtIndex:0] processIdentifier];
+    
+    NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"FinderExt" ofType:@"bundle"];
+    
+    NSLog(@"Bundle path: %@", bundlePath);
+    NSLog(@"PID: %d", pid);
+    
+    mach_error_t err;
+    err = mach_inject_bundle_pid([bundlePath fileSystemRepresentation], pid);
+    if (err == err_none) {
+      NSLog(@"Inject successful!");
+    } else {
+      NSLog(@"Inject error: %d", err);
+    }
+  }
+  return 0;
 }
 
